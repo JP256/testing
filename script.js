597f7281284f9a24178e825b88658213bd45fa54
Javier Pena <j.p256@yahoo.com>
2022-04-26 15:38:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//Basic Callback //////////////////////////
+function doSomething(callback) {
+  console.log("dooo");
+  callback();
+}
+function doMore() {
+  console.log("more");
+}
+// doSomething(doMore);
+///////////////////////////////////////////
+function one() {
+  setTimeout(() => {
+    console.log("hi");
+  }, 500);
+}
+function two() {
+  console.log("bye");
+}
+//print firs two and then two, behaving asynchronous
+// one();
+// two();
+// the right way is using callbacks
+function three(callback) {
+  setTimeout(() => {
+    console.log("hi");
+    callback();
+  }, 500);
+}
+function four() {
+  console.log("bye");
+}
+three(four);
+////// Promesas
+function miP() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(console.log("hii"));
+    }, 3000);
+  });
+}
+function five() {
+  console.log("bye");
+}
+miP()
+  .then(() => {
+    five();
+  })
+  .then(() => console.log("siii"));
